da6bcfd4fc169a01b53c9c361746e6bcda0164cd
LEVYABDI <kevyabdi@gmail.com>
2025-08-13 23:26:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,52 @@
-import re, os
+import os
 
-id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
-API_HASH = os.environ.get("API_HASH", "")
-#BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-TOKEN_ONE = os.environ.get("TOKEN_ONE", "")
 
-CHANNEL = os.environ.get("CHANNEL", "ATL_Univers") # username without '@'
-BOT_USERNAME = os.environ.get("BOT_USERNAME","") # username without '@'
-SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP","BYNF_TamilChat") # username without '@'
-UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL","ATL_Univers") # username without '@'
-OWNER_USERNAME = os.environ.get("OWNER_USERNAME","HMF_Owner_1")
-STRING = os.environ.get("STRING", "")
+# Required Variables Config
+API_ID = int(os.environ.get("API_ID", "27965918"))
+API_HASH = os.environ.get("API_HASH", "d5ca408334552615fa7e8f48c2dac999")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8305685426:AAFf4mTOULfYy46VyeoxoI2YqADfPgokTD0")
+ADMIN = int(os.environ.get("ADMIN", "1096693642"))
 
-DB_NAME = os.environ.get("DB_NAME","renameone")     
-DB_URL = os.environ.get("DB_URL","")
 
-FLOOD = int(os.environ.get("FLOOD", "90"))
-LAZY_PIC = os.environ.get("LAZY_PIC", "")
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1989750989').split()]
-PORT = os.environ.get('PORT', '8080')
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001230390281"))
+# Premium 4GB Renaming Client Config
+STRING_SESSION = os.environ.get("STRING_SESSION", "BAGqud4AspJrjkXrlVjsIyNFfesTMHQE3T_A9arnx1tcHDbFl7-ImWPMdDhEbxnIW6H1ZHp3Ya9Y9gi4--y3x2eoAsykBVl909FF1Ge5BiMlJC6VEukjrHbsIbk4CirYv3kUE4hTCqXRdu8x1TXDfddmJD1VrXHrQ4hVhyl7Q719X8XxwuZ0vq_zccPVVYrQpT3hcvN6AqplVBs5LtkIkHZXfW0vxhF8oFLEo7DZBJPHYVC8PDddsx_1RYHiQUnRTR90qtpPy16pPNYfjC7NrwMRDpTvsVge1VU7_0uLvwAkZ3_w5pLjtj5D4otb406c4rTayh_ncsLuzLWiEK3OOPt4qGme5wAAAAFOw4TnAA")
+
+
+# Log & Force Channel Config
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "daarotv")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002071039887"))
+
+
+# Mongo DB Database Config
+DATABASE_URL = os.environ.get("DATABASE_URL", "BAGqud4AspJrjkXrlVjsIyNFfesTMHQE3T_A9arnx1tcHDbFl7-ImWPMdDhEbxnIW6H1ZHp3Ya9Y9gi4--y3x2eoAsykBVl909FF1Ge5BiMlJC6VEukjrHbsIbk4CirYv3kUE4hTCqXRdu8x1TXDfddmJD1VrXHrQ4hVhyl7Q719X8XxwuZ0vq_zccPVVYrQpT3hcvN6AqplVBs5LtkIkHZXfW0vxhF8oFLEo7DZBJPHYVC8PDddsx_1RYHiQUnRTR90qtpPy16pPNYfjC7NrwMRDpTvsVge1VU7_0uLvwAkZ3_w5pLjtj5D4otb406c4rTayh_ncsLuzLWiEK3OOPt4qGme5wAAAAFOw4TnAA")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "kevyabdi20")
+
+
+# Other Variables Config
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
+
+
+
+
+
+# If You Need To Add Verify System Then Message Me On Telegram
+# Check Demo Bots
+# https://t.me/FileRenameXBot
+# https://t.me/PremiumRenamerRobot
+# https://t.me/FileRenamerXRobot
+# Token Verification Adding Features Is Paid So If You Want Then Dm Me
+
+
+# SHORTNER_URL = os.environ.get("SHORTNER_URL", "")
+# SHORTNER_API = os.environ.get("SHORTNER_API", "")
+# TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "")
+
+
+
+
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @Madflix_Bots
+# Backup Channel @JishuBotz
+# Developer @JishuDeveloper
